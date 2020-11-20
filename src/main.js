52307744219e52ed6db11de1676b337877f25870
eanx <sergio.ferlito@enea.it>
2020-11-20 19:20:22 +0100

    added store

--- src/main.js
+++ src/main.js
@@ -1,40 +1,42 @@
-import Vue from "vue";
-import App from "./App.vue";
-import Axios from "./plugins/axios";
-import router from "./router";
-import { Auth0Plugin } from "./auth";
-import HighlightJs from "./directives/highlight";
+import Vue from 'vue'
+import App from './App.vue'
+import Axios from './plugins/axios'
+import router from './router'
+import { Auth0Plugin } from './auth'
+import HighlightJs from './directives/highlight'
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faLink, faUser, faPowerOff } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faLink, faUser, faPowerOff } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-import { domain, clientId, audience } from "../auth_config.json";
+import { domain, clientId, audience } from '../auth_config.json'
 
+import store from './store/store'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 Vue.use(Auth0Plugin, {
   domain,
   clientId,
   audience,
-  onRedirectCallback: (appState) => {
+  onRedirectCallback: appState => {
     router.push(
       appState && appState.targetUrl
         ? appState.targetUrl
         : window.location.pathname
-    );
-  },
-});
+    )
+  }
+})
 
-Vue.use(Axios);
-Vue.directive("highlightjs", HighlightJs);
+Vue.use(Axios)
+Vue.directive('highlightjs', HighlightJs)
 
-library.add(faLink, faUser, faPowerOff);
+library.add(faLink, faUser, faPowerOff)
 
-Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 new Vue({
   router,
-  render: (h) => h(App),
-}).$mount("#app");
+  store,
+  render: h => h(App)
+}).$mount('#app')
